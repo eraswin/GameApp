4ea08f50d57e77101ac6a1745929524d3e3e4efa
eraswin <ult.sofis@gmail.com>
2024-09-21 20:35:49 +0530

    Initial Commit

---
+++ screens/StartGame.js
@@ -0,0 +1,12 @@
+import { TextInput, View } from "react-native";
+
+function StartGame() {
+    return (
+        <View>
+            <TextInput />
+            BUTTONS
+        </View>
+    );
+}
+
+export default StartGame;
