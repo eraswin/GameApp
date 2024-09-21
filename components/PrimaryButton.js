e630881a1e729d5c8a48849d95feff39ef68856b
eraswin <ult.sofis@gmail.com>
2024-09-21 21:29:45 +0530

    53 reached

---
+++ components/PrimaryButton.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+function PrimaryButton ({ childern }) {
+    return (
+        <View>
+            <Text>{childern}</Text>
+        </View>
+    );
+}
+
+export default PrimaryButton;
--- screens/StartGame.js
+++ screens/StartGame.js
@@ -1,12 +1,47 @@
-import { TextInput, View } from "react-native";
+import { TextInput, View, StyleSheet } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
 
 function StartGame() {
     return (
-        <View>
-            <TextInput />
-            BUTTONS
+        <View style={styles.inputContainer}>
+            <TextInput 
+                style={styles.numberInput} 
+                maxLength={2} 
+                keyboardType="number-pad"
+                autoCapitalize="none"
+                autoFocus={true}
+                autoCorrect={false}
+            />
+            <PrimaryButton childern={'Reset'} />
+            <PrimaryButton childern={'Confirm'} />
         </View>
     );
 }
 
 export default StartGame;
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        marginTop: 100,
+        marginHorizontal: 24,
+        padding: 16,
+        backgroundColor: '#72063c',
+        borderRadius: 8,
+        elevation: 5,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 6,
+        shadowOpacity: 0.25,
+    },
+    numberInput: {
+        height: 50,
+        width: 50,
+        fontSize: 32,
+        borderBottomColor: '#ddb52f',
+        borderBottomWidth: 2,
+        color: '#ddb52f',
+        marginVertical: 8,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    }
+});
